c787b62bdefe6c8768e5136d1b6e6f9fbff8fc43
ilagomatis <hliaslago@gmail.com>
2023-08-16 13:57:25 +0000

    unpack function

---
+++ src/ops/image_classification.c
@@ -0,0 +1,54 @@
+#include "image_classification.h"
+#include "error.h"
+#include "plugin.h"
+#include "log.h"
+#include "vaccel_ops.h"
+#include "genop.h"
+
+#include "session.h"
+
+int vaccel_custom_image_classification(struct vaccel_session *sess, char* model_path,
+		char* image_path, char* labels_path, float input_mean, float input_std,
+		char* output)
+{
+	if (!sess)
+		return VACCEL_EINVAL;
+
+	vaccel_debug("session:%u Looking for plugin implementing IMAGE_CLASSIFICATION operation",
+			sess->session_id);
+
+	//Get implementation
+	int (*plugin_op)() = get_plugin_op(IMAGE_CLASSIFICATION);
+	if (!plugin_op)
+		return VACCEL_ENOTSUP;
+
+	return plugin_op(sess, model_path, image_path, labels_path,
+			input_mean, input_std, output);
+}
+
+int vaccel_custom_image_classification_unpack(struct vaccel_session *sess, struct vaccel_arg *read,
+		int nr_read, struct vaccel_arg *write, int nr_write)
+{
+	if (nr_read != 5) {
+		vaccel_error("Wrong number of read arguments in IMAGE_CLASSIFICATION: %d",
+				nr_read);
+		return VACCEL_EINVAL;
+	}
+
+	if (nr_write != 1) {
+		vaccel_error("Wrong number of write arguments in IMAGE_CLASSIFICATION: %d",
+				nr_write);
+		return VACCEL_EINVAL;
+	}
+
+	char* model_path  =  *(char**)read[0].buf;
+	char* image_path  =  *(char**)read[1].buf;
+	char* labels_path =  *(char**)read[2].buf;
+	float input_mean  =  *(float*)read[3].buf;
+	float input_std   =  *(float*)read[4].buf;
+
+	char* output      =  *(char**)write[0].buf;
+
+	return vaccel_custom_image_classification(sess, model_path, image_path, labels_path,
+			input_mean, input_std, output);
+}
---
+++ src/ops/image_classification.h
@@ -0,0 +1,15 @@
+#ifndef __IMAGE_CLASSIFICATION_H__
+#define __IMAGE_CLASSIFICATION_H__
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "include/ops/image_classification.h"
+
+struct vaccel_session;
+struct vaccel_arg;
+
+int vaccel_custom_image_classification_unpack(struct vaccel_session *sess, struct vaccel_arg *read,
+		int nr_read, struct vaccel_arg *write, int nr_write);
+
+#endif 
